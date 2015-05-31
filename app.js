4a6c39e96e8616eba170cff9d27271fcabd03194
ulnda <denis.nagibin@gmail.com>
2015-05-31 17:55:16 +0300

    Photos template, dummy photos, routing

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var photos = require('./routes/photos');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('photos', __dirname + '/public/photos');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,8 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', photos.list);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
